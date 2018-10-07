69c5b3b2cd4626158bc5bc87092386df427d1e16
Sergey Tvarnov <brop@mail.ru>
2018-10-07 23:44:49 +0300

    lesson9: VIew->render()

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,9 +4,5 @@ namespace app\controllers;
 
 class MainController extends AppController {
   public function indexAction() {
-    echo 'Main controller';
-    debug($this->route);
-    debug($this->route['controller']);
-    die;
   }
 }
---
+++ app/views/layouts/default.php
@@ -0,0 +1,9 @@
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  Это шаблон
+ <?=$content?>  
+</body>
+</html>
