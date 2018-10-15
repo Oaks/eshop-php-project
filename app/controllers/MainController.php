e91950877e6641947ac5374e8e8840a7c2412e84
Sergey Tvarnov <brop@mail.ru>
2018-10-15 23:49:52 +0300

    lesson12: ORM RedBeanPHP

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -6,9 +6,11 @@ use ishop\App;
 
 class MainController extends AppController {
   public function indexAction() {
+    $posts = \R::findAll('test');
+    $post = \R::findOne('test', 'id=?', [2]);
     $this->setMeta('Главная страница', 'Описание ...', 'Ключевики ');
     $name = 'John';
     $age = 30;
-    $this->set(compact('name', 'age'));
+    $this->set(compact('name', 'age', 'posts'));
   }
 }
--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,9 +1,17 @@
 <head>
   <meta charset="UTF-8">
   <?=$this->getMeta();?>
+
 </head>
 <body>
   Это шаблон
  <?=$content?>  
+  <?php
+    $logs = \R::getDatabaseAdapter()
+      ->getDatabase()
+      ->getLogger();
+
+    debug( $logs->grep('SELECT') );
+  ?>
 </body>
 </html>
