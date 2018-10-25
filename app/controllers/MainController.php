953249c4657d81241e8756cb7426ae7da900f862
Sergey Tvarnov <brop@mail.ru>
2018-10-26 00:31:52 +0300

    Part 2. lesson17: Product hits display

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,7 +8,8 @@ use ishop\Cache;
 class MainController extends AppController {
   public function indexAction() {
     $brands = \R::find('brand', 'limit 3');
+    $hits = \R::find('product', "hit='1' AND status='1' LIMIT 8");
     $this->setMeta('Главная страница', 'Описание ...', 'Ключевики');
-    $this->set(compact('brands'));
+    $this->set(compact('brands', 'hits'));
   }
 }
