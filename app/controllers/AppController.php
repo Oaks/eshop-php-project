686eaaa84337a598c0df8a0950d9eedae4c0f11b
Sergey Tvarnov <brop@mail.ru>
2018-10-27 00:06:42 +0300

    Part 2. lesson18: Widget currency select (part 1)

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -3,11 +3,17 @@
 namespace app\controllers;
 
 use ishop\base\Controller;
+use ishop\App;
 use app\models\AppModel;
+use app\widgets\currency\Currency;
 
 class AppController extends Controller {
   public function __construct( $route ) {
     parent::__construct($route);
     new AppModel();
+    //setcookie('currency', 'EUR', time()+3600*24*7, '/');
+    App::$app->setProperty('currencies', Currency::getCurrencies());
+    App::$app->setProperty('currency', Currency::getCurrency(App::$app->getProperty('currencies')));
+    debug(App::$app->getProperties());
   }
 }
