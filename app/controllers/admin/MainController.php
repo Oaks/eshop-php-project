7d7316765107f05b91d0629e7871a408192e8d5a
Sergey Tvarnov <brop@mail.ru>
2018-12-30 15:40:01 +0200

    Part 3. lesson56: Main page widgets

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -4,5 +4,18 @@ namespace app\controllers\admin;
 
 class MainController extends AppController {
   public function indexAction() {
+    $countNewOrders = \R::count('order', "status='0'");
+    debug($countNewOrders);
+    $countUsers = \R::count('user');
+    $countProducts = \R::count('product');
+    $countCategories = \R::count('category');
+    $this->set(compact(
+      'countNewOrders',
+      'countUsers',
+      'countProducts',
+      'countCategories'
+    ));
+    $this->setMeta('Панель управления');
+
   }
 }
